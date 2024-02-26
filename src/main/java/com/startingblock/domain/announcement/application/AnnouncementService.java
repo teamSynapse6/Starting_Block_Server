4d6f4528a1213b8959336793aab825e727392662
Sejin Park <95167215+sejineer@users.noreply.github.com>
2024-02-26 15:29:24 +0900

    fix: 공고 리스트 조회 API 쿼리 수정 (#7)
    * feat: 공고 조건별 조회 API 구현
    
    * fix: 공고 조회 API 쿼리 수정

--- src/main/java/com/startingblock/domain/announcement/application/AnnouncementService.java
+++ src/main/java/com/startingblock/domain/announcement/application/AnnouncementService.java
@@ -1,17 +1,17 @@
 package com.startingblock.domain.announcement.application;
 
+import com.startingblock.domain.announcement.dto.AnnouncementDetailRes;
 import com.startingblock.domain.announcement.dto.AnnouncementRes;
 import com.startingblock.global.config.security.token.UserPrincipal;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 public interface AnnouncementService {
 
-    Void refreshAnnouncements();
+    void refreshAnnouncements();
     Slice<AnnouncementRes> findAnnouncements(UserPrincipal userPrincipal, Pageable pageable, String businessAge, String region, String supportType, String sort, String search);
+    AnnouncementDetailRes findAnnouncementDetailById(Long announcementId);
 
 }
