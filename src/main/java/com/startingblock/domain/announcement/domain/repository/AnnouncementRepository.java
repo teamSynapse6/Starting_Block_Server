c3ba4de0da74b282320192aeaebc9cdd29c68f04
Gyehwan Baek <b99hwan@gmail.com>
2024-05-31 02:09:40 +0900

    feat: 교외 지원 공고 이메일 크롤링 자동화

--- src/main/java/com/startingblock/domain/announcement/domain/repository/AnnouncementRepository.java
+++ src/main/java/com/startingblock/domain/announcement/domain/repository/AnnouncementRepository.java
@@ -2,13 +2,8 @@ package com.startingblock.domain.announcement.domain.repository;
 
 import com.startingblock.domain.announcement.domain.Announcement;
 import com.startingblock.domain.announcement.domain.AnnouncementType;
-import com.startingblock.domain.announcement.domain.Keyword;
-import com.startingblock.domain.announcement.domain.University;
-import com.startingblock.domain.announcement.dto.SupportGroupRes;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 
@@ -17,5 +12,7 @@ public interface AnnouncementRepository extends JpaRepository<Announcement, Long
     List<Announcement> findAnnouncementsByAnnouncementTypeAndIsFileUploaded(AnnouncementType announcementType, boolean isFileUsed);
 
     List<Announcement> findByAnnouncementType(AnnouncementType announcementType);
+
+    List<Announcement> findByAnnouncementTypeAndContactIsNull(AnnouncementType announcementType);
 
 }
--- src/main/java/com/startingblock/domain/crawling/application/CrawlingService.java
+++ src/main/java/com/startingblock/domain/crawling/application/CrawlingService.java
@@ -16,6 +16,7 @@ import com.startingblock.domain.crawling.oncampus.SungKyunKwanUniv;
 import com.startingblock.domain.crawling.oncampus.YonSeiUniv;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -53,5 +54,14 @@ public class CrawlingService {
     public void offCampusEmailCrawling() {
         OpenData openData = new OpenData();
         openData.offCampusEmailCrawling(announcementRepository.findByAnnouncementType(AnnouncementType.OPEN_DATA));
+    }
+
+    // TODO: k-startup 공고 이메일 크롤링 자동화
+    @Transactional
+    @Scheduled(cron = "0 30 3 * * ?") // 매일 새벽 3시 30분에 실행
+    public void offCampusEmailAutoCrawling() {
+        log.info("k-startup 공고 이메일 크롤링 자동화 시작");
+        OpenData openData = new OpenData();
+        openData.offCampusEmailCrawling(announcementRepository.findByAnnouncementTypeAndContactIsNull(AnnouncementType.OPEN_DATA));
     }
 }
